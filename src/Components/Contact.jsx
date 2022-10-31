e9b1d22f00424d0ce1c1bb5b4ddc32b976ac986c
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2022-10-31 12:12:42 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,60 +4,31 @@ import CONTACT from '../images/contact.png'
 const Contact = () => {
   return (
     <>
-    <div className="container-fluid back-image-contact">
-      <div className="container" id="contact">
-        <div className="row con">
-          <div className="col-10 mx-auto">
-            <h5 className="text-center display-4">Contact Me</h5>
-            <div className="row">
-            <div className="col-md-6 contact">
-                  <img src={CONTACT} alt="SIDE BANNER" className="img-fluid" />
-            </div>
-              <div className="col-md-6">
-                <form>
-                  <div className="mb-3">
-                    <label htmlFor="userName" className="form-label">
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      name="username"
-                      className="form-control"
-                      id="userName"
-                      placeholder="Enter Your Full Name"
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      name="useremail"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      placeholder="Enter Your E-Mail"
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">
-                      Enter Your Message
-                    </label>
-                    <textarea
-                      className="form-control"
-                      name="usermessage"
-                      id="exampleFormControlTextarea1"
-                      rows="3"
-                      placeholder="Your Message"
-                    ></textarea>
-                  </div>
-                  <input type="submit" className="btn btn-primary" value='SUBMIT'/>
-                </form>
-              </div>
-            </div>
+    <div className="container-fluid ">
+        <div className="container" id="contact">
+        <div className="row">
+       
+      <div className="form-box">
+      <h1 className="display-4 text-center contact-heading">Contact Us</h1>
+      <img src={CONTACT} className="img-fluid contact-img" alt="contact-us" />
+
+      <form action="https://formsubmit.co/d1c6012d19ef63b27156f9fec97fc328" method="POST">
+          <div className="form-group">
+            <label htmlFor="name">NAME</label>
+            <input className="form-control" id="name" type="text" name="name" required/>
           </div>
+          <div className="form-group">
+            <label htmlFor="email">EMAIL</label>
+            <input className="form-control" id="email" type="email" name="email" required/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">MESSAGE</label>
+            <textarea className="form-control" id="message" name="message"></textarea>
+          </div>
+          <input className="btn btn-primary" type="submit" value="Submit" />
+        </form>
         </div>
+      </div>
       </div>
       </div>
     </>
